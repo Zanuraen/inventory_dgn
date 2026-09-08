f1e2f65efa392ea8543d5e3074bcc7526a88bd8a
zanuraen <zanuraen.88@gmail.com>
2026-09-08 10:36:18 +0800

    tes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AssetController;
 use App\Http\Controllers\AssetHandoverController;
 
+// === fitur dashboard ====
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+// === fitur data barang ====
 Route::resource('assets', AssetController::class);
 Route::get('assets/{asset}/detail', [AssetController::class, 'detail'])->name('assets.detail');
 Route::post('assets/{asset}/handovers', [AssetHandoverController::class, 'store'])->name('handovers.store');
